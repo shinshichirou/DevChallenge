039d5c075d77a6c38b0d51bf85d1c99fc9a67cb7
Igor Tudoran <igortudoran@me.com>
2018-10-16 12:53:32 +0300

    Initial Commit

---
+++ DevChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevChallenge
+//
+//  Created by Igor Tudoran on 10/16/18.
+//  Copyright © 2018 Igor Tudoran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
